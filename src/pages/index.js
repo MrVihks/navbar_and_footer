94c7f8cece4cf47e7bfa1c2171fc7dc229907e7e
Vinicius Henrique <79711257+MrVihks@users.noreply.github.com>
2022-09-30 12:31:41 -0300

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import Styles from '../styles/Home.module.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
    <>
+    <Head>
+      <title>navbar-footer-sample</title>
+    </Head>
     <div id={Styles.content}>
       <h1>Isto é um exemplo 🤔</h1>
       <a>😎</a>
